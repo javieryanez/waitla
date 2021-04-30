d3f7ba7262b0236dda7f8c5279f99bc09c43b7db
Javier Yanez <jyanez747@gmail.com>
2021-04-30 20:23:18 +0200

    Added set sleep time option

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ use num_cpus;
 fn main() {
     let mut treshold: f32 = 0.0;
     let mut reverse: bool = false;
+    let mut sleep_millis: u64 = 1000;
 
-    setup(&mut treshold, &mut reverse);
+    setup(&mut treshold, &mut reverse, &mut sleep_millis);
 
-    wait(&mut treshold, &mut reverse);
+    wait(&mut treshold, &mut reverse, &mut sleep_millis);
 }
 
-fn wait(treshold: &mut f32, reverse: &mut bool) {
+fn wait(treshold: &mut f32, reverse: &mut bool, sleep_millis: &mut u64) {
     if !*reverse {
         println!("Waiting load average less than {}", treshold);
     } else {
@@ -27,7 +28,7 @@ fn wait(treshold: &mut f32, reverse: &mut bool) {
     let sys = System::new();
     let mut la = get_load_average(&sys);
     while (la > *treshold && !*reverse) || (la < *treshold && *reverse){
-        thread::sleep(Duration::from_millis(1000));
+        thread::sleep(Duration::from_millis(*sleep_millis));
         la = get_load_average(&sys);
     }
 }
@@ -43,7 +44,7 @@ fn get_load_average(sys: &System) -> f32 {
     return result;
 }
 
-fn setup(treshold: &mut f32, reverse: &mut bool) {
+fn setup(treshold: &mut f32, reverse: &mut bool, sleep_millis: &mut u64) {
     // Prints each argument on a separate line
     for argument in env::args() {
         println!("{}", argument);
@@ -70,11 +71,33 @@ fn setup(treshold: &mut f32, reverse: &mut bool) {
                 .required(false)
                 .about("Waits the load average to be major than the treshold, rather than lower"),
         )
+        .arg(
+            Arg::new("sleep_millis")
+                .short('s')
+                .long("sleep-millis")
+                .takes_value(true)
+                .required(false)
+                .about("Set sleep time in milliseconds"),
+        )
         .get_matches();
 
     set_treshold(&matches, treshold);
 
     set_reverse(&matches, reverse);
+
+    set_sleep_time(&matches, sleep_millis);
+}
+
+fn set_sleep_time(matches: &ArgMatches, sleep_millis: &mut u64) {
+    let sleep_time_str = matches.value_of("sleep_millis").unwrap();
+
+    match sleep_time_str.parse::<u64>() {
+        Ok(s) => *sleep_millis = s,
+        Err(_) => {
+            println!("That's not a valid sleep time! {}", sleep_time_str);
+            process::exit(0x001);
+        }
+    }
 }
 
 fn set_reverse(matches: &ArgMatches, reverse: &mut bool) {
